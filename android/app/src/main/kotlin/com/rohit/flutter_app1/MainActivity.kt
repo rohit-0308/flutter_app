07f4c45394c01f9ed99950b44fa01a799b9d0086
rohit-0308 <rohitkumar860311@gmail.com>
2020-11-10 21:41:44 +0530

    Learning Flutter

---
+++ android/app/src/main/kotlin/com/rohit/flutter_app1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rohit.flutter_app1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('EasyList'),
+          ),
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () {
+                    setState(() {
+                      _products.add('Advanced Food Tester');
+                    });
+                  },
+                  child: Text('Add Product'),
+                ),
+              ),
+              Column(
+                children: _products
+                    .map(
+                      (e) => Card(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset('assets/food.jpg'),
+                            Text(e),
+                          ],
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+            ],
+          )),
+    );
+  }
+}
